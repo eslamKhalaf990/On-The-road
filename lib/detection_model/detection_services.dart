36d75c021e0ef08e8623157cc8792488cb92f239
Abdelrahman Hussain <a.elrahman.hussain@gmail.com>
2023-06-19 12:14:47 +0300

    distance is working

--- lib/detection_model/detection_services.dart
+++ lib/detection_model/detection_services.dart
@@ -2,69 +2,65 @@ import 'package:camera/camera.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_pytorch/pigeon.dart';
 
-class detectionServices {
-  var objectsWidth = {'Car': 1.9, 'tv': 0.51, 'laptop': 0.51};
-  var objectsToReport = ['Stop Sign', 'Speed Pump'];
-  var reportAccuarcy = 0.85;
-  var ttsAccuarcy = 0.80;
+class DetectionServices {
+  final objectsWidth = {'Car': 1.9, 'tv': 0.51, 'laptop': 0.51};
+  final objectsToReport = ['Stop Sign', 'Speed Pump'];
+  final reportAccuracy = 0.85;
+  final ttsAccuracy = 0.80;
   final channel = MethodChannel('java_channel');
-  var safeDistanceTime = 2.0; //2 seconds
+  final safeDistanceTime = 2.0; // 2 seconds
   double focalLength = 0;
 
-  objectDetected(
+  void objectDetected(
       CameraImage cameraImage, List<ResultObjectDetection> objDetect) {
     for (var obj in objDetect) {
       if (objectsToReport.contains(obj.className) &&
-          obj.score >= reportAccuarcy) {
+          obj.score >= reportAccuracy) {
         sendToServer(cameraImage);
       }
-      if (objectsWidth.containsKey(obj.className) && obj.score >= ttsAccuarcy) {
-        double distance = calculateDistance(obj) as double;
-        double safeDistance = calculateSafeDistance();
-        if (distance > safeDistance) {
-          notKeepingSafeDistance(obj.className!, distance);
-        }
+      if (objectsWidth.containsKey(obj.className) && obj.score >= ttsAccuracy) {
+        calculateDistance(obj).then((distance) {
+          double safeDistance = calculateSafeDistance();
+          if (distance > safeDistance) {
+            notKeepingSafeDistance(obj.className!, distance);
+          }
+        });
       }
     }
-    //tts
-    //or send to server
+    // tts
+    // or send to server
   }
 
   Future<double> calculateDistance(ResultObjectDetection object) async {
-    var FL = await getFocalLength();
-    double distance = double.parse(
-        ((0.51 * FL * 0.5) / object.rect.width).toStringAsFixed(1));
+    double FL = await getFocalLength();
+    double distance = (0.51 * FL * 0.25) / object.rect.width;
+    print(distance);
     return distance;
-    double focalLength = getFocalLength();
-    return double.parse(
-        ((objectsWidth[object.className]! * focalLength) / object.rect.width)
-            .toStringAsFixed(1));
   }
 
-  calculateSafeDistance() {
+  double calculateSafeDistance() {
     var currentSpeed = 0.0;
-    //get current speed in meters
+    // get current speed in meters
     return safeDistanceTime * currentSpeed;
   }
 
-  getFocalLength() async {
+  Future<double> getFocalLength() async {
     if (focalLength == 0) {
-      double FL;
-      FL = await channel.invokeMethod('getFocalLength');
+      double FL = await channel.invokeMethod('getFocalLength');
       focalLength = FL;
     }
     return focalLength;
   }
 
-  sendToServer(CameraImage cameraImage) {
-    print("sending to server...");
+  void sendToServer(CameraImage cameraImage) {
+    print('Sending to server...');
   }
 
-  notKeepingSafeDistance(String obj, double distance) {
-    tts("there is a $obj in $distance Please keep your safe distance");
+  void notKeepingSafeDistance(String obj, double distance) {
+    tts('There is a $obj at $distance. Please maintain a safe distance.');
   }
 
-  tts(String text) {
-    //call text to speech method
+  void tts(String text) {
+    // call text-to-speech method
   }
 }
