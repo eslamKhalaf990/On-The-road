946d513b21244a1c5d1139e36f1e04f056f290fe
Abdelrahman Hussain <a.elrahman.hussain@gmail.com>
2023-07-07 18:32:21 +0300

    settings edited

--- lib/model/settings.dart
+++ lib/model/settings.dart
@@ -2,29 +2,69 @@ import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-class SettingsModel extends ChangeNotifier{
-  late LocationAccuracy locationAccuracy = LocationAccuracy.bestForNavigation;
-  late MapType mapTheme = MapType.normal;
+class SettingsModel extends ChangeNotifier {
+  LocationAccuracy locationAccuracy = LocationAccuracy.bestForNavigation;
+  MapType mapTheme = MapType.normal;
+  double notifyDistance = 0.0;
+  double safeDistanceTime = 2.0;
+  bool askAboutObjects = false;
+  bool voiceAssistEnabled = false;
 
   List<String> settings = [
     "Location Accuracy",
-    "Map Theme"
+    "Map Theme",
+    "Notify Distance",
+    "Safe Distance",
+    "Ask About Objects on Road",
+    "Voice Assist",
   ];
-  List<IconData>icons = [
+  List<IconData> icons = [
     Icons.location_searching_outlined,
     Icons.map_outlined,
+    Icons.notification_important,
+    Icons.timer,
+    Icons.question_answer,
+    Icons.volume_up,
   ];
-  List<List>options = [
-    ["HIGH", "MEDIUM", "LOW"],
-    [const AssetImage("images/ic_launcher.png"), const AssetImage("images/ic_launcher_color.png")],
+  List<List> options = [
+    [
+      "HIGH",
+      "MEDIUM",
+      "LOW",
+    ],
+    [
+      const AssetImage("images/ic_launcher.png"),
+      const AssetImage("images/ic_launcher_color.png"),
+    ],
   ];
 
-  setLocationAccuracy(LocationAccuracy accuracy){
+  void setLocationAccuracy(LocationAccuracy accuracy) {
     locationAccuracy = accuracy;
     notifyListeners();
   }
-  setMapTheme(MapType type){
+
+  void setMapTheme(MapType type) {
     mapTheme = type;
+    notifyListeners();
+  }
+
+  void setNotifyDistance(double distance) {
+    notifyDistance = distance;
+    notifyListeners();
+  }
+
+  void setSafeDistanceTime(double time) {
+    safeDistanceTime = time;
+    notifyListeners();
+  }
+
+  void setAskAboutObjects(bool value) {
+    askAboutObjects = value;
+    notifyListeners();
+  }
+
+  void setVoiceAssistEnabled(bool value) {
+    voiceAssistEnabled = value;
     notifyListeners();
   }
 }
--- lib/view/settings/settings.dart
+++ lib/view/settings/settings.dart
@@ -6,22 +6,44 @@ import 'package:provider/provider.dart';
 import '../../constants/design_constants.dart';
 import '../../ai_models/tree_accelaration/gyroscope.dart';
 
-class SettingsView extends StatelessWidget {
-
+class SettingsView extends StatefulWidget {
   SettingsView({Key? key}) : super(key: key);
+
+  @override
+  _SettingsViewState createState() => _SettingsViewState();
+}
+
+class _SettingsViewState extends State<SettingsView> {
   final gyroscope gyro = gyroscope();
+  bool isGyroscopeOn = false;
+
+  @override
+  void initState() {
+    super.initState();
+    isGyroscopeOn = gyro.working;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Consumer<SettingsModel>(
       builder: (BuildContext context, settingsModel, child) {
-        return Scaffold(
-          body: Column(
-            children: [
-              ListView.builder(
-                shrinkWrap: true,
-                itemCount: settingsModel.settings.length,
-                itemBuilder: (BuildContext context, int index) {
-                  return Container(
+        return SafeArea(
+          child: Scaffold(
+            body: SingleChildScrollView(
+              child: Column(
+                children: [
+                  Container(
+                    margin: const EdgeInsets.all(20),
+                    child: const Text(
+                      "SETTINGS",
+                      style: TextStyle(
+                          fontSize: 35,
+                          fontFamily: 'tajawal',
+                          fontWeight: FontWeight.w900),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                  Container(
                     height: 130,
                     padding: const EdgeInsets.all(10),
                     margin: const EdgeInsets.all(10),
@@ -29,86 +51,249 @@ class SettingsView extends StatelessWidget {
                     child: Column(
                       children: [
                         ListTile(
-                          leading: Icon(settingsModel.icons[index]),
-                          title: Text(settingsModel.settings[index]),
+                          leading: Icon(settingsModel.icons[0]),
+                          title: Text(settingsModel.settings[0]),
                         ),
                         SizedBox(
                           height: 50,
                           child: ListView.builder(
-                              shrinkWrap: true,
-                              scrollDirection: Axis.horizontal,
-                              itemCount: settingsModel.options[index].length,
-                              itemBuilder: (BuildContext ctx, k) {
-                                return Container(
-                                  margin:
-                                      const EdgeInsets.only(right: 1, left: 1),
-                                  child: Row(
-                                    children: [
-                                      settingsModel.options[index][k] is String
-                                          ? MaterialButton(
-                                              onPressed: () {
-                                                print(
-                                                  settingsModel
-                                                      .options[index].length,
-                                                );
-                                                if (index == 0 && k == 0) {
-                                                  settingsModel
-                                                          .locationAccuracy =
-                                                      LocationAccuracy
-                                                          .bestForNavigation;
-                                                } else if (index == 0 &&
-                                                    k == 1) {
-                                                  settingsModel
-                                                          .locationAccuracy =
-                                                      LocationAccuracy.lowest;
-                                                }
-                                              },
-                                              child: Text(settingsModel
-                                                  .options[index][k]))
-                                          : MaterialButton(
-                                              onPressed: () {
-                                                if (index == 1 && k == 0) {
-                                                  settingsModel.mapTheme =
-                                                      MapType.normal;
-                                                } else if (index == 1 &&
-                                                    k == 1) {
-                                                  settingsModel.mapTheme =
-                                                      MapType.satellite;
-                                                }
-                                              },
-                                              child: ClipRRect(
-                                                borderRadius:
-                                                    const BorderRadius.all(
-                                                  Radius.circular(13),
-                                                ),
-                                                child: Image(
-                                                  height: 45,
-                                                  image: settingsModel
-                                                      .options[index][k],
-                                                ),
-                                              ),
-                                            ),
-                                    ],
-                                  ),
-                                );
-                              }),
-                        )
+                            shrinkWrap: true,
+                            scrollDirection: Axis.horizontal,
+                            itemCount: settingsModel.options[0].length,
+                            itemBuilder: (BuildContext ctx, k) {
+                              return Container(
+                                margin:
+                                    const EdgeInsets.only(right: 1, left: 1),
+                                child: Row(
+                                  children: [
+                                    if (settingsModel.options[0][k] is String)
+                                      MaterialButton(
+                                        onPressed: () {
+                                          // if (index == 0 && k == 0) {
+                                          //   settingsModel.locationAccuracy =
+                                          //       LocationAccuracy
+                                          //           .bestForNavigation;
+                                          // } else if (index == 0 && k == 1) {
+                                          //   settingsModel.locationAccuracy =
+                                          //       LocationAccuracy.lowest;
+                                          // }
+                                        },
+                                        child:
+                                            Text(settingsModel.options[0][k]),
+                                      )
+                                    else
+                                      MaterialButton(
+                                        onPressed: () {
+                                          // if (index == 1 && k == 0) {
+                                          //   settingsModel.mapTheme =
+                                          //       MapType.normal;
+                                          // } else if (index == 1 && k == 1) {
+                                          //   settingsModel.mapTheme =
+                                          //       MapType.satellite;
+                                          // }
+                                        },
+                                        child: ClipRRect(
+                                          borderRadius: const BorderRadius.all(
+                                            Radius.circular(13),
+                                          ),
+                                          child: Image(
+                                            height: 45,
+                                            image: settingsModel.options[0][k],
+                                          ),
+                                        ),
+                                      ),
+                                  ],
+                                ),
+                              );
+                            },
+                          ),
+                        ),
                       ],
                     ),
-                  );
-                },
+                  ),
+                  Container(
+                    height: 130,
+                    padding: const EdgeInsets.all(10),
+                    margin: const EdgeInsets.all(10),
+                    decoration: DesignConstants.roundedBorder,
+                    child: Column(
+                      children: [
+                        ListTile(
+                          leading: Icon(settingsModel.icons[1]),
+                          title: Text(settingsModel.settings[1]),
+                        ),
+                        SizedBox(
+                          height: 50,
+                          child: ListView.builder(
+                            shrinkWrap: true,
+                            scrollDirection: Axis.horizontal,
+                            itemCount: settingsModel.options[1].length,
+                            itemBuilder: (BuildContext ctx, k) {
+                              return Container(
+                                margin:
+                                    const EdgeInsets.only(right: 1, left: 1),
+                                child: Row(
+                                  children: [
+                                    if (settingsModel.options[1][k] is String)
+                                      MaterialButton(
+                                        onPressed: () {
+                                          // if (index == 0 && k == 0) {
+                                          //   settingsModel.locationAccuracy =
+                                          //       LocationAccuracy
+                                          //           .bestForNavigation;
+                                          // } else if (index == 0 && k == 1) {
+                                          //   settingsModel.locationAccuracy =
+                                          //       LocationAccuracy.lowest;
+                                          // }
+                                        },
+                                        child:
+                                            Text(settingsModel.options[1][k]),
+                                      )
+                                    else
+                                      MaterialButton(
+                                        onPressed: () {
+                                          // if (index == 1 && k == 0) {
+                                          //   settingsModel.mapTheme =
+                                          //       MapType.normal;
+                                          // } else if (index == 1 && k == 1) {
+                                          //   settingsModel.mapTheme =
+                                          //       MapType.satellite;
+                                          // }
+                                        },
+                                        child: ClipRRect(
+                                          borderRadius: const BorderRadius.all(
+                                            Radius.circular(13),
+                                          ),
+                                          child: Image(
+                                            height: 45,
+                                            image: settingsModel.options[1][k],
+                                          ),
+                                        ),
+                                      ),
+                                  ],
+                                ),
+                              );
+                            },
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Container(
+                    height: 130,
+                    padding: const EdgeInsets.all(10),
+                    margin: const EdgeInsets.all(10),
+                    decoration: DesignConstants.roundedBorder,
+                    child: ListTile(
+                      leading: const Icon(Icons.gesture),
+                      title: const Text('Gyroscope'),
+                      trailing: Switch(
+                        value: isGyroscopeOn,
+                        onChanged: (value) {
+                          setState(() {
+                            isGyroscopeOn = value;
+                          });
+                          if (value) {
+                            gyro.start();
+                          } else {
+                            gyro.end();
+                          }
+                        },
+                      ),
+                    ),
+                  ),
+                  Container(
+                    height: 130,
+                    padding: const EdgeInsets.all(10),
+                    margin: const EdgeInsets.all(10),
+                    decoration: DesignConstants.roundedBorder,
+                    child: ListTile(
+                      leading: const Icon(Icons.notification_important),
+                      title: const Text('Notify Distance'),
+                      trailing: SizedBox(
+                        width: 120,
+                        child: TextFormField(
+                          initialValue: settingsModel.notifyDistance.toString(),
+                          keyboardType: TextInputType.number,
+                          onChanged: (value) {
+                            settingsModel.notifyDistance = double.parse(value);
+                          },
+                          decoration: const InputDecoration(
+                            hintText: 'Enter distance',
+                            contentPadding: EdgeInsets.zero,
+                            isDense: true,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    height: 130,
+                    padding: const EdgeInsets.all(10),
+                    margin: const EdgeInsets.all(10),
+                    decoration: DesignConstants.roundedBorder,
+                    child: ListTile(
+                      leading: const Icon(Icons.timer),
+                      title: const Text('Safe Distance'),
+                      trailing: ToggleButtons(
+                        borderRadius: BorderRadius.circular(8),
+                        selectedColor: Colors.white,
+                        fillColor: Colors.blue,
+                        children: const [
+                          Padding(
+                            padding: EdgeInsets.symmetric(horizontal: 16),
+                            child: Text('2 sec'),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.symmetric(horizontal: 16),
+                            child: Text('3 sec'),
+                          ),
+                        ],
+                        isSelected: [
+                          settingsModel.safeDistanceTime == 2.0,
+                          settingsModel.safeDistanceTime == 3.0,
+                        ],
+                        onPressed: (index) {
+                          settingsModel.safeDistanceTime = [2.0, 3.0][index];
+                        },
+                      ),
+                    ),
+                  ),
+                  Container(
+                    height: 130,
+                    padding: const EdgeInsets.all(10),
+                    margin: const EdgeInsets.all(10),
+                    decoration: DesignConstants.roundedBorder,
+                    child: ListTile(
+                      leading: const Icon(Icons.question_answer),
+                      title: const Text('Ask About Objects on Road'),
+                      trailing: Switch(
+                        value: settingsModel.askAboutObjects,
+                        onChanged: (value) {
+                          settingsModel.askAboutObjects = value;
+                        },
+                      ),
+                    ),
+                  ),
+                  Container(
+                    height: 130,
+                    padding: const EdgeInsets.all(10),
+                    margin: const EdgeInsets.all(10),
+                    decoration: DesignConstants.roundedBorder,
+                    child: ListTile(
+                      leading: const Icon(Icons.volume_up),
+                      title: const Text('Voice Assist'),
+                      trailing: Switch(
+                        value: settingsModel.voiceAssistEnabled,
+                        onChanged: (value) {
+                          settingsModel.voiceAssistEnabled = value;
+                        },
+                      ),
+                    ),
+                  ),
+                ],
               ),
-              TextButton(
-                  onPressed: () {
-                    if (gyro.working) {
-                      gyro.end();
-                    } else {
-                      gyro.start();
-                    }
-                  },
-                  child: const Text("Gyroscope"),
-              )
-            ],
+            ),
           ),
         );
       },
